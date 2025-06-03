67f8895af24e06ccb617fa5d24f54ec3b0143956
panicat855 <ocpagos25@gmail.com>
2025-06-02 22:06:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.getElementById("loginForm")?.addEventListener("submit", function(e) {
     e.preventDefault();
     const user = document.getElementById("user").value;
     const pass = document.getElementById("pass").value;
-
     if (user === "admin" && pass === "admin123") {
         window.location.href = "dashboard.html";
     } else {
